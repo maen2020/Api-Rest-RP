b9b9c70c59d4b4625b7681f133c146e613eccb8c
Miguel Angel <miguelangel@192.168.1.12>
2023-09-26 18:03:18 -0600

    Project start

---
+++ src/main/java/com/maen/ApiRestRpApplication.java
@@ -0,0 +1,12 @@
+package com.maen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestRpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestRpApplication.class, args);
+	}
+}
